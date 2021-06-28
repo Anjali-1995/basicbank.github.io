81a142b56a96f0f3a592694c2b505f14c4349816
Anjali-1995 <84699762+Anjali-1995@users.noreply.github.com>
2021-06-28 11:50:41 +0530

    commit change

--- config.php
+++ config.php
@@ -1,9 +1,15 @@
 <?php
 	//Local Development Connection requirement
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "tsf-bank";
+// 	$servername = "localhost";
+// 	$username = "root";
+// 	$password = "";
+// 	$dbname = "tsf-bank";
+
+$dbname="8OsSHp0px5";
+$servername = "remotemysql.com";
+
+$username = "8OsSHp0px5";
+$password = "F4jScZyhkH";
 
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 
